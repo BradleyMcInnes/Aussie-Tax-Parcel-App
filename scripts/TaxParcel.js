dbd3003cb3fc5530ce9eb520c17c7490d26794c8
Bradley Mcinnes <b_mcinnes@hotmail.com>
2018-01-02 07:54:40 +0000

    Played with JS Still not working

--- scripts/TaxParcel.js
+++ scripts/TaxParcel.js
@@ -4,6 +4,8 @@ function delta(){
     var TargetName = document.getElementById('TargetName').value;
     var CurrentUnits = Number(document.getElementById('CurrentUnits').value);
     var TargetUnits = Number(document.getElementById('TargetUnits').value);
+    var OutputName = document.getElementById('OutputName').value;
+    var OutputDelta = document.getElementById('OutputDelta').value;
 
     if(CurrentName==" " || TargetName==" " || CurrentUnits==" " || TargetUnits==" "){
     	alert('One or more inputs is empty');
@@ -13,6 +15,8 @@ function delta(){
     	}
     	else{
     	var delta = TargetUnits - CurrentUnits;
-    	alert('The difference is ' + delta);
+        //alert(delta);
+    	document.getElementById('OutputName').innerHTML = "Test";
+        document.getElementById('OutputDelta').innerHTML = "test";
     	}    				
 }
